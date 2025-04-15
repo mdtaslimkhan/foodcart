2096bb076145442e033d320f80d9a81c578e51cd
mdtaslimkhan <asalam773@gmail.com>
2025-04-16 00:36:47 +0600

    init files

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { DrawerActions, NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Dashboard from './screen/dashboard';
+import Home from './screen/home';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+
+   
+  const Drawer = createDrawerNavigator();
+
+
+  const Stacknav = () => {
+    const Stack = createNativeStackNavigator();
+    const navigation = useNavigation();
+    const handlePress = () => {
+      console.log("asdfhgo");
+    }
+
+    const handlePressecond = () => {
+      console.log("second item");
+    }
+
+    return (
+    <Stack.Navigator 
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerRight: () => {
+          return <View style={styles.navRightIcon}>
+            <TouchableOpacity style={styles.navitem} onPress={handlePress}>
+              <AntDesign name='windowso' size={26} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navitem} onPress={handlePressecond}>
+              <AntDesign name='printer' size={26} />
+            </TouchableOpacity>
+            </View>
+          },
+        }}>
+        <Stack.Screen name='Dashboard'
+        component={Dashboard}
+        options={{
+          headerLeft: () => {
+            return(
+              <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer()) }>
+                <AntDesign name='windowso' size={26} color="black" /> 
+              </TouchableOpacity>
+            );
+          }
+        }}
+         />
+        <Stack.Screen name='Home' component={Home} />
+    </Stack.Navigator>
+    );
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator screenOptions={{
+        headerShown: false }}>
+        <Drawer.Screen name='Dashboard'
+         component={Stacknav}
+          />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  navRightIcon: {
+    flexDirection: 'row',
+    
   },
+  navitem: {
+    marginLeft: 16
+  }
 });
+
